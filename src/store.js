341b0b124595ee06f4df6fd665f819d2174cc9e0
Alee02 <a-shah@hotmail.co.uk>
2019-06-09 01:22:06 +0100

    RegisterViewComponent and register action

--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    userData: null
+  },
+  mutations: {
+    SET_USER_DATA (state, payload) {
+      state.userData = payload
+    }
+  },
+  actions: {
+    register ({ commit }, credentials) {
+      return axios
+        .post('http://localhost:3000/register', credentials)
+        .then(({ data }) => {
+          console.log('user data is', data)
+          commit('SET_USER_DATA', data)
+        })
+      }
+  }
 })
